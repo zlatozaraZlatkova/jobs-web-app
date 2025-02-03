6f386c0917ae523454fbacbce2ef036473de0bc0
Zara <zl.zlatkova@gmail.com>
2025-02-03 20:35:01 +0100

    Add Create Company Route

--- Rest-api/server/controllers/employerProfileController.js
+++ Rest-api/server/controllers/employerProfileController.js
@@ -1,19 +1,45 @@
 const router = require("express").Router();
-
+const { body, validationResult } = require("express-validator");
 
 const { hasUser, checkUserRole } = require("../middlewares/guards");
 const { errorParser } = require("../util/errorParser");
-const { getUserById } = require("../services/employerProfileService");
+const { createItem, getUserById, getCompanyByUserId } = require("../services/employerProfileService");
 
 router.post("/create", hasUser(), checkUserRole("employer"),
-  
+    body("name", "Name is required").not().isEmpty(),
+    body("description", "Please enter a description up to 3000 characters long").isLength({ max: 3000 }),
+    body("contactEmail", "Email is required").not().isEmpty(),
+    body("contactPhone", "Phone is required").not().isEmpty(),
     async (req, res) => {
         const userId = req.user._id;
- 
-        try {
-            let userProfile = await getUserById(userId);
+        const errors = validationResult(req);
 
-            res.status(201).json(userProfile);
+        if (!errors.isEmpty()) {
+            const message = errorParser(errors.array());
+            return res.status(400).json({ message });
+        }
+
+
+        const { name, description, contactEmail, contactPhone } = req.body;
+
+        const company = {
+            name,
+            description,
+            contactEmail,
+            contactPhone,
+            ownerId: userId,
+        };
+
+        try {
+          
+            const existingCompany = await getCompanyByUserId(userId);
+            if (existingCompany) {
+                return res.status(400).json({ message: "Company profile already exists." });
+            }
+ 
+            const createCompany = await createItem(userId, company);
+
+            res.status(201).json(createCompany);
 
         } catch (error) {
             console.log(error);
--- Rest-api/server/services/employerProfileService.js
+++ Rest-api/server/services/employerProfileService.js
@@ -1,15 +1,28 @@
 const EmployerProfile = require("../models/EmployerProfileSchema");
-
+const Company = require("../models/Company");
 
 async function getUserById(id) {
   return EmployerProfile.findById(id);
 }
 
+async function getCompanyByUserId(userId) {
+  return Company.findOne({ ownerId: userId });
+ }
+ 
 
+async function createItem(userId, data) {
+  const newCompany = await Company.create(data);
 
+  const employerProfile = await EmployerProfile.create({
+    companyId: newCompany._id,
+    ownerId: userId,
+  });
 
+  return { company: newCompany, profile: employerProfile };
+}
 
 module.exports = {
   getUserById,
-  
+  createItem,
+  getCompanyByUserId
 };
